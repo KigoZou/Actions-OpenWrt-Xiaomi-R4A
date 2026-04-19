467f0de91e97c6d23a8df45c5a580f60a42f03ad
Kigo_Zou <Kigo_Zou@163.com>
2026-04-19 20:03:34 +0800

    移除多余

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,10 +26,6 @@ sed -i 's_downloads.openwrt.org_mirrors.ustc.edu.cn/openwrt_' package/lean/defau
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 修改主题文件
-# rm -rf package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
 ### 修改为R4A千兆版Breed直刷版
 ## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
 ## 1.修改 mt7621_xiaomi_mir3g-v2.dts
